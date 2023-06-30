cd295a50c6ece068582d449db05e03f67cd67033
wahyunf354 <wahyunurfadillah313@gmail.com>
2023-06-30 20:25:36 +0700

    membuat tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instragram_clone_ui/pages/home_page.dart';
 
 void main () => runApp(MyApp());
 
@@ -12,12 +13,47 @@ class MyApp extends StatelessWidget {
       home: DefaultTabController(
         length: 5,
         child: Scaffold(
+          body: TabBarView(
+            children: [
+              HomePage(),
+              Center(
+                child: Text("Search"),
+              ),
+              Center(
+                child: Text("Reels"),
+              ),
+              Center(
+                child: Text("Shopping"),
+              ),
+              Center(
+                child: Text("Profile"),
+              ),
+            ],
+          ),
           bottomNavigationBar: Container(
             color: Colors.black,
             child: TabBar(
+              indicatorColor: Colors.black,
               tabs: [
                 Tab(
                   icon: Icon(Icons.home_filled),
+                ),
+                Tab(
+                  icon: Icon(Icons.search),
+                ),
+                Tab(
+                  icon: Image.asset('assets/images/ic_reels.png', width: 24,),
+                ),
+                Tab(
+                  icon: Image.asset('assets/images/ic_shopping_bag.png', width: 24,),
+                ),
+                Tab(
+                  icon: SizedBox(
+                    width: 24,
+                    child: ClipOval(
+                      child: Image.asset('assets/images/img_profile.png')
+                    ),
+                  ),
                 )
               ],
             ),
